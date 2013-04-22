5096575676b3b0d78e238d64f007c14f83760abd
DITTBERNER, Matthias <mdittber@student.ethz.ch>
2013-04-22 17:28:17 +0200

    new gui_simulate, directly table to qdot

---
+++ MATLAB/Functions/delProgressInfo.m
@@ -0,0 +1,14 @@
+function delProgressInfo(fObject, hObject)
+%delProgressInfo(msg, fObject, hObject)
+%   fObject:    figure, where the information
+%               will be displayed (gui_simulate,...)
+%   hObject:    Tag (as string) of the object in the figure,
+%               where the information will be displayed
+%               (t_progess,...)
+
+guiData = guidata(fObject);
+handle  = getfield(guiData, hObject);
+set(handle,'String','');
+
+end
+
