c064a1e25a9ae4af9b2718101ce2b54e8fa29826
gra13f2 <gra13f2@badile-k61-6c.ee.ethz.ch>
2013-04-22 20:18:02 +0200

    init: automatic user recognition

--- MATLAB/System/init.m
+++ MATLAB/System/init.m
@@ -3,10 +3,12 @@
 
 global config;
 
-if strcmp(getenv('USER'), 'gra13f2')
+if strcmp(getenv('USERNAME'), 'gra13f2')
     config.root = '/usr/zupo/stud7/gra13f2/GroupProject/'
-else
+elseif strcmp(getenv('USER'), 'christianfunck')
     config.root    = '/Users/christianfunck/Data/ETH/ITET/GA/GroupProject/';
+elseif strcmp(getenv('USERNAME'), 'matthias')
+    config.root    = 'C:\Users\matthias\Documents\ETH\ITET\Gruppenarbeit\';
 end
 
 config.system  = [config.root, 'MATLAB/System/'];
@@ -15,8 +17,11 @@ config.user    = getenv('USER');
 config.machine = getenv('HOSTNAME');
 config.OMEN   = [config.root, 'OMEN_ethz-amd64'];
 config.simulations = [config.root, 'Simulations/'];
-
 config.vOMEN = 'version';
+
+if exist(config.simulations) == 0
+    mkdir(config.simulations)
+end
 
 addpath(genpath(config.root))
 cd(config.root)
