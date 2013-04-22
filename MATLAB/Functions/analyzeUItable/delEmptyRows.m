9d3fa6495ef8ded46bb353ab22973723f4dd994f
DITTBERNER, Matthias <mdittber@student.ethz.ch>
2013-04-22 17:28:17 +0200

    new gui_simulate, directly table to qdot

---
+++ MATLAB/Functions/analyzeUItable/delEmptyRows.m
@@ -0,0 +1,11 @@
+function [Mpar] = delEmptyRows(Mpar_raw)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+    e = cellfun('isempty', Mpar_raw)
+        e = sum(e,2);
+        [idx,e] = find(e);
+        Mpar_raw(idx,:) = [];
+Mpar = Mpar_raw;
+end
+
