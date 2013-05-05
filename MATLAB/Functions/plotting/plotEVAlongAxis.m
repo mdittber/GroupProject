36ff4eae1dcab6174063e4d20ed842d4375182fa
Christian Funck <cfunck@ethz.ch>
2013-05-05 15:40:18 +0200

    plotEV3D: multiple dots, subplots. plotEVAlongAxis: ok'

--- MATLAB/Functions/plotting/plotEVAlongAxis.m
+++ MATLAB/Functions/plotting/plotEVAlongAxis.m
@@ -1,76 +1,80 @@
-function plotEVsAlongAxis(dots, pName, startPoint, direction)
-    % plot the EVs of multiple Qdots along specified line
+function plotEVAlongAxis(dots, pName, startPoint, direction, plotGrid, tolerance, NMod)
+    % plot the EVs of multiple Qdots along specified line.
+    % pName is the for the plot relevant Qdot property.
+    % set plotGrid = [indices] OR 'all' for visualisation of Qdot and specified line. 
+    % tolerance in %/100 of a0. (for latticetype 'zincblende': will be /2)
+    
+    Ndots = length(dots); 
+    pValue = cell(1,Ndots);
+    LayerMatrix = cell(1,Ndots);
+    EV = cell(1,Ndots);
+    tol = zeros(Ndots,1);
+    psi2 = cell(1,Ndots);
+    selAtomInd = cell(1,Ndots);
+    scale = cell(1,Ndots);
+    subset = cell(1,Ndots); 
 
-    % get EVs and param values for pName
+% get EVs and param values for pName
     
-    
-%     N=length(dots);
-%     EV = cell(N,1);
-%     pValue = zeros(N,1);
-%     for i=1:N
-%         EV{i} = load( [dots(i).path, '/VB_V_0_0.dat'] );
-%         pValue(i) = eval( sprintf('dots(%i).%s',i,pName) ); 
-%     end
-    
-    NN=2;
-    
-    a = 'ID20130429-1213-34315_CdS_CdSe';
-    b = 'ID20130426-1803-05541_PbSe_allan';
-    
-%     dots(1) = load( ['Simulations/' a '/qdotObj.mat'] );
-%     dots(2) = load( ['Simulations/' b '/qdotObj.mat'] );
+    for k =1:Ndots
+        %     simPath = [config.Simulations, qdotObj.path];
+        simPath = sprintf('Simulations/ID%s_%s/', dots(k).timestamp, dots(k).mat_name);
 
-    
-    EV{1} = load( ['Simulations/' a '/VB_V_0_0.dat'] );
-    EV{2} = load( ['Simulations/' b '/VB_V_0_0.dat'] );
-    
-    LayerMatrix{1} = load( ['Simulations/' a '/Layer_Matrix.dat'] );
-    LayerMatrix{2} = load( ['Simulations/' b '/Layer_Matrix.dat'] );
-
-    psi2 = cell(1,NN);
-    selAtomInd = cell(1,NN);
-    scale = cell(1,NN);
-    
-    
-    
-    
-    % set different tolerances for different directions        
-    ndir = direction/norm(direction);
-    
-    if isequal(ndir, [1 0 0]) || isequal(ndir, [0 1 0]) || isequal(ndir, [0 0 1]) 
-        tol = 0.01;
-    else
-        tol = 0.2;
+        LayerMatrix{k} = load([simPath, 'Layer_Matrix.dat']);
+%         CB_V = load([simPath, 'CB_V_0_0.dat']);
+        
+        EV{k} = load([simPath, 'VB_V_0_0.dat']);
+        
+        pValue{1,k} = eval( sprintf('dots(%i).%s',k,pName) );
+        
+        tol(k) = dots(k).a0 * tolerance;
+        if strcmp(dots(k).lattice_type, 'zincblende')
+            tol(k) = tol(k)/2;
+        end
     end
+        
+% get indices and scale for atoms on specified line
     
-    
-    %get indices and scale for atoms on specified line
-    
-    for k =1:NN
+    for k =1:Ndots
         %calc nr of orbitals
         [NAtom,~] = size(LayerMatrix{k});            
         [n,~]  = size( EV{k} );
         NOrb = n/NAtom; %nr of orbitals
         
         %calc psi2
-        psi2{k} = EV2psi( EV{k}, NOrb, 1);
+        psi2{k} = EV2psi( EV{k}, NOrb, NMod);
         
-        %get 
-        [selAtomInd{k}, scale{k}] = atomsOnAxis(LayerMatrix{k}, startPoint, direction, tol);
+        %get atoms on line
+        [selAtomInd{k}, scale{k}] = atomsOnAxis(LayerMatrix{k}, startPoint, direction, tol(k));
+        
+        %print warning if few atoms on line
+        if length(selAtomInd{k})<8
+            warning('ID %i: Less than 8 atoms on line! Set plotGrid = true to check the line',k);
+        end
 
     end
     
-    subset = cell(1,NN); 
-    
-    for k = 1:NN
-        subset{k} = psi2{k}(selAtomInd{k}); % psi2 for atoms on line
-        
-        highlightAtoms(LayerMatrix{k}, selAtomInd{k}, startPoint, direction, 100+k); %plot atoms and line
-        
+% plot atoms and line if selected in plotGrid
+    if isequal(plotGrid, 'all')
+        plotGrid = 1:Ndots;
+    elseif isequal(plotGrid, 0)
+        plotGrid = [];
     end
     
-    plotNormal(subset, 'mat', [1,2], scale, 3); % plot wavefn's along spec line
-
+    for k = plotGrid
+        plotID = 10+k;
+        figure(plotID);
+        title( sprintf('ID %i', k) );
+        highlightAtoms(LayerMatrix{k}, selAtomInd{k}, startPoint, direction, plotID); %plot atoms and line    
+    end
+    
+% plot wavefn's for each dot and mode
+    for i = 1:NMod
+        for k = 1:Ndots
+            subset{k} = psi2{k}(selAtomInd{k},i); % psi2 for atoms on line for dot k and mode i
+        end
+        plotNormal(subset, pName, pValue, scale, i);
+    end
 end
 
 
@@ -83,20 +87,22 @@ function highlightAtoms(grid, atoms, startPoint, direction, plotid)
     % optional plotting of a line
     
     [NAll,~] = size(grid);    
+    direction = direction/norm(direction);
+    
     figure(plotid); 
     hold on
     
-    %plot all atoms in grid
+%plot all atoms in grid
     for k =1:NAll
         plot3(grid(k,1), grid(k,2), grid(k,3), 'ko')
     end
     
-    %plot only hitAtoms
+%plot only hitAtoms
     for k =1:length(atoms)
         plot3(grid(atoms(k),1), grid(atoms(k),2), grid(atoms(k),3), 'r*')
     end
-    
-    t = [-1,1];
+        
+    t = [-1,1] * max(max(grid(:,1:3)));
     line = startPoint'*[1,1] + direction'*t;
 
     plot3(line(1,:), line(2,:), line(3,:), 'b-')
@@ -120,7 +126,7 @@ function [sortedIndices, scale] = atomsOnAxis(LayerMatrix, startPoint, direction
     direction = direction/norm(direction); %normalize
     distance = @(a,n,p) norm( (a-p) - ((a-p)'*n)*n ); % distance point to line. n normalised!
 
-    %set default values for tol and startPoint
+%set default values for tol and startPoint
     if nargin < 4
         tol = 0.1;
     end
@@ -128,7 +134,7 @@ function [sortedIndices, scale] = atomsOnAxis(LayerMatrix, startPoint, direction
         startPoint = [0,0,0];
     end
 
-    % extract indices of atoms close to specified line
+% extract indices of atoms close to specified line
     for i = 1:N
         if distance(startPoint', direction', atomGrid(i,:)') < tol 
             indices(end+1,1) = i;
@@ -136,7 +142,7 @@ function [sortedIndices, scale] = atomsOnAxis(LayerMatrix, startPoint, direction
         end
     end
     
-    % sort indices along axis (using projection on axis) 
+% sort indices along axis (using projection on axis) 
     sorted = sortrows([indices, proj],2); %sort (dist)
     
     sortedIndices = sorted(:,1);
@@ -155,10 +161,11 @@ function plotNormal(values, pName, pValue, scales, plotid)
     % optional scale. default: aequidistant values
     
     N = length(values);
-    
+
+% set scales
     if nargin < 4 %set default scale (uniform spacing)
         for k = 1:N
-            m = length( values{k} )
+            m = length( values{k} );
             x{1,k}= linspace(0,1,m);
         end
         
@@ -166,12 +173,17 @@ function plotNormal(values, pName, pValue, scales, plotid)
         x = cellfun(@(a) (a-a(1))/ abs(a(end)-a(1)), scales, 'UniformOutput', 0); %normalise scale    
     end
     
+% styles and labels
     allStyles = {'-k','-r','-g','-b','-c',':k',':r',':g',':b',':c','--k','--r','--g','--b','--c'};
     style = allStyles(1:N);
-    labels = arrayfun( @(b) sprintf('%s = %g',pName,b), pValue, 'UniformOutput', false);
+    IDs = num2cell(1:N);
+    labels = cellfun( @(a,b) sprintf('ID %i: %s', a, mat2str(b)), IDs, pValue, 'UniformOutput', false);
 
     figure(plotid);
+    title( sprintf('Parameter: %s', pName), 'Interpreter', 'none');
     hold on;
+
+% plot    
     h = cellfun(@(x,y,st) plot(x,y,st), x, values, style);
-    legend(h,labels);
+    legend(h,labels, 'Interpreter', 'none');
 end    
