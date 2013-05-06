b75106e7a45c8a031a6b86ea369a7aaaede8addf
Christian Funck <cfunck@ethz.ch>
2013-05-06 19:26:18 +0200

    plotEValongAvis: scatter3 and colorcode for different materials. Bands can be specified. DButils: filter functions: findDiff, findRatio.

--- MATLAB/Functions/plotting/plotBandGap.m
+++ MATLAB/Functions/plotting/plotBandGap.m
@@ -6,33 +6,33 @@ function plotBandGap()
     [DB, SimDirs] = DButils.createDB();
     [n,m] = size(DB);
     for i=1:m
-        [BGap(i), Radius(i,:), Volt(i), Mat(i)] = getBandGap(DB(i),SimDirs(i).name)
+        [BGap(i), Radius(i,:), Volt(i), Mat(i)] = getBandGap(DB(i),SimDirs(i).name);
     end
     
     figure;
     hold on;
-    [n,m] = size(Radius)
+    [n,m] = size(Radius);
     if m == 1
         Col = Volt/1;%max(Volt)
-        S = 250*Mat
-        sVolt = round(sort(unique(Volt))*100)/100
-        sh = scatter(Radius(:,1),BGap,S,Col)
+        S = 250*Mat;
+        sVolt = round(sort(unique(Volt))*100)/100;
+        sh = scatter(Radius(:,1),BGap,S,Col);
         xlabel('Radius of Quantum Dot in nm');
         ylabel('Band Gap in eV');
         hcb = colorbar('Location','SouthOutside','XTickLabel',sVolt);
         xlabel(hcb, 'Voltage in V');
-        set(hcb,'XTickMode','manual')
+        set(hcb,'XTickMode','manual');
         
         %% TO DO different circle sizes
     else
         %% TO DO: Only for one Voltage!!!
         Col = BGap/max(BGap);
         S = 25*Mat;
-        scatter3(Radius(:,1),Radius(:,2),BGap,S,Col)
+        scatter3(Radius(:,1),Radius(:,2),BGap,S,Col);
         xlabel('Inner radius of Quantum Dot in nm');
         ylabel('Outer radius of Quantum Dot in nm');
         hcb = colorbar('Location','SouthOutside','XTickLabel',{'Band Gap in eV'; Col});
-        set(hcb,'XTickMode','manual')
+        set(hcb,'XTickMode','manual');
     end
 
 % 
