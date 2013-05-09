ad02ffdb0307d6a651abeffad9556eafd810c172
Christian Funck <cfunck@ethz.ch>
2013-05-09 12:38:49 +0200

    Figures Added. Bug in compareEV fixed. QDStructureplot: function for multiple qdotobjs

--- MATLAB/Functions/plotting/QDStructurePlot.m
+++ MATLAB/Functions/plotting/QDStructurePlot.m
@@ -1,20 +1,28 @@
-%% Ploting the structure of a Quantum Dot in 3D
+function QDStructurePlot(qdotObj)
+% Ploting the structure of a Quantum Dot in 3D
 
-NA = size(Layer_Matrix,1);
-numOfElements = max(Layer_Matrix(:,4));
-SizeCode = 20*ones(1,NA)';
-ColCode = Layer_Matrix(:,4)/numOfElements;
+    global config;
 
-fig_count = 1;
-%fig = figure(fig_count)
-scatter3(Layer_Matrix(:,1), Layer_Matrix(:,2), Layer_Matrix(:,3),SizeCode,ColCode,'filled');
-title('Structure of a XYZ quantum dot');
-%legend('Pb','S');
-xlabel('x-axis');
-ylabel('y-axis');
-zlabel('z-axis');
-%A(1,1) = 5;
-%refresh(H)
+    for k = 1:length(qdotObj)
+    
+        Layer_Matrix = load( [config.simulations, qdotObj.path, '/Layer_Matrix.dat']);
 
-%saveas(fig, getTimeDate);
+        NA = size(Layer_Matrix,1);
+        numOfElements = max(Layer_Matrix(:,4));
+        SizeCode = 30*ones(1,NA)';
+        ColCode = Layer_Matrix(:,4)/numOfElements;
 
+        fig_count = 1;
+        %fig = figure(fig_count)
+        scatter3(Layer_Matrix(:,1), Layer_Matrix(:,2), Layer_Matrix(:,3),SizeCode,ColCode,'filled');
+        title('Structure of a XYZ quantum dot');
+        %legend('Pb','S');
+        xlabel('x-axis');
+        ylabel('y-axis');
+        zlabel('z-axis');
+        %A(1,1) = 5;
+        %refresh(H)
+
+        %saveas(fig, getTimeDate);
+    end
+end
