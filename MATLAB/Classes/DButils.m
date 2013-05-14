c1cd6e8962ffa86eaf3001f6e1446f8670de190c
gra13f2 <gra13f2@badile-k61-6c.ee.ethz.ch>
2013-05-14 17:12:35 +0200

    DButils: delete function added

--- MATLAB/Classes/DButils.m
+++ MATLAB/Classes/DButils.m
@@ -27,6 +27,15 @@ classdef DButils
             
         end
         
+        function  delete(DB)
+        % WARNING: Deletes the simulation folders and their contents of the objects in DB
+            global config;
+            for k = 1:length(DB)
+                dir = [config.simulations, DB(k).path];
+                rmdir( dir, 's' );
+            end
+        end
+        
         function updateProperty(DIR, pName, value)
         %% updateProperty(DIR, pName, value)
         %   DIR     directory path relative to 'root/Simulations/'
